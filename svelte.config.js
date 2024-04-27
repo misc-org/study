215f3b4bdc91740bb749464d855f9214625d5c0c
wappon28dev <wappon_28_dev@ruru.be>
2024-04-27 23:51:31 +0900

    🔧 adapter static に変更

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: "dist",
-			assets: "dist",
-			fallback: undefined,
-			precompress: false,
-			strict: true,
-		}),
+  kit: {
+    adapter: adapter({
+      pages: "dist",
+      assets: "dist",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
 
-		paths: {
-			base: production ? '/study' : '',
-		},
-	},
-
-	onwarn: (warning, handler) => {
-		const { code } = warning;
-
-		if (code === "css-unused-selector") {
-			return;
-		}
-
-		handler(warning);
-	},
+    paths: {
+      base: production ? "/study" : "",
+    },
+  },
 };
 
 export default config;
