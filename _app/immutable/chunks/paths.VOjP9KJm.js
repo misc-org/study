497768a247e58ca62ae0af41014e102f4b606501
wappon28dev <wappon28dev@users.noreply.github.com>
2024-04-28 15:38:03 +0000

    deploy: d5c469e2ddd64bdc64bc1367f5c95bee59973df0

---
+++ _app/immutable/chunks/paths.VOjP9KJm.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1rel3sq)==null?void 0:s.base)??"/study";var e;const a=((e=globalThis.__sveltekit_1rel3sq)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/entry/start.sSMOrlQZ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CY447dFA.js";export{t as start};
