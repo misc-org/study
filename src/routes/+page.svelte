ea704672f9a511f4ebf379f38bca2977fdae9530
shiki-01 <0110shiki@gmail.com>
2024-04-29 14:59:15 +0900

    ♻️ update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,33 @@
 <section>
   {#each data.blogList.contents as blog}
     <article>
-      <a href="{base}/blogs/{blog.id}"><h2>{blog.title}</h2></a>
+      <h2><a href="{base}/blogs/{blog.id}">{blog.title}</a></h2>
     </article>
   {/each}
 </section>
+
+<style lang="scss">
+  @import "svelte-materialify/src/styles/tools/colors";
+  article {
+    margin: 1em 0;
+  }
+
+  h2 {
+    font-size: 1.5em;
+    margin: 1em 0;
+    border-bottom: 4px solid material-color("light-blue", "lighten-4");
+
+    a {
+      text-decoration: none;
+    }
+
+    @media (max-width: 768px) {
+      font-size: 1.2em;
+    }
+
+    &::before {
+      content: "#";
+      margin-right: 0.5em;
+    }
+  }
+</style>
