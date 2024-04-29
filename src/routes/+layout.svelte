4868c59cf5dfa344ee55987d1105db1fea166fe2
shiki-01 <0110shiki@gmail.com>
2024-04-29 10:16:37 +0900

    📁 save 全体レイアウトの変更

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import { onMount } from "svelte";
+  import Icon from "@iconify/svelte";
+  import { setupViewTransition } from "sveltekit-view-transition";
+
+  setupViewTransition();
+
+  let lastScrollY = 0;
+  let isScrollingDown = false;
+
+  onMount(() => {
+    window.addEventListener("scroll", () => {
+      const currentScrollY = window.pageYOffset;
+      isScrollingDown = currentScrollY > lastScrollY;
+      lastScrollY = currentScrollY;
+    });
+  });
 </script>
 
 <main>
-  <header>
-    <a href="https://misc-org.github.io/study/">Home</a>
+  <header style=" top: {isScrollingDown ? '-50px' : '0'}; ">
+    <a href="{base}/"
+      ><Icon icon="material-symbols:home-app-logo" height={30} /></a
+    >
   </header>
   <div>
     <slot />
@@ -12,26 +30,44 @@
 </main>
 
 <style lang="scss">
+  :global(body) {
+    margin: 0;
+    background-color: #f0f0f0;
+  }
+
   main {
     font-family: "M PLUS 1p", sans-serif;
 
     header {
-      background-color: #f3db00;
+      width: 100%;
+      background-color: #6deeff;
       padding: 1em 5em;
       color: #1f1f1f;
+      position: fixed;
+      transition: top 0.3s;
 
       a {
-        color: #1f1f1f;
+        color: rgb(29, 76, 95);
         text-decoration: none;
-        font-size: 1.5em;
-        &:hover {
-          text-decoration: underline;
-        }
+        display: flex;
+        align-items: center;
+        width: fit-content;
+      }
+
+      @media (max-width: 768px) {
+        padding: 1em 1.5em;
       }
     }
 
     div {
+      margin-top: 5em;
       padding: 1em 5em;
+    }
+
+    @media (max-width: 768px) {
+      div {
+        padding: 2em;
+      }
     }
   }
 </style>
--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -1,11 +1,53 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   export let data: PageData;
+
+  import { onMount, onDestroy } from 'svelte';
+  let isExpanded = false;
+  let expandedImgSrc = '';
+
+  let observer: MutationObserver;
+
+  onMount(() => {
+    observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+          const images = document.querySelectorAll('img');
+          images.forEach((img) => {
+            img.addEventListener('click', () => {
+              isExpanded = true;
+              expandedImgSrc = img.src;
+            });
+          });
+        }
+      });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+  });
+
+  onDestroy(() => {
+    if (observer) {
+      observer.disconnect();
+    }
+  });
+
+  function closeOverlay() {
+    isExpanded = false;
+  }
 </script>
 
 <section>
   <h1>{data.detail.title}</h1>
-  <div><span>{@html data.detail.content}</span></div>
+  <div>
+    <div class="overlay {isExpanded ? 'show' : ''}" on:click={closeOverlay}>
+      {#if isExpanded}
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img class="expanded-img" src={expandedImgSrc} />
+      {/if}
+    </div>
+    <span>{@html data.detail.content}</span>
+  </div>
 </section>
 
 <style lang="scss">
@@ -13,13 +55,12 @@
     padding: 1em 5em;
 
     h1 {
-      font-family: "M PLUS 1p", sans-serif;
-      font-weight: 100;
       margin: 1em 0;
       margin-bottom: 1.5em;
-      background-color: #f3db00;
+      background-color: #6deeff;
       padding: 0.5em 1em;
       color: #1f1f1f;
+      border-radius: 0.2em;
     }
 
     div {
@@ -28,6 +69,18 @@
       align-items: center;
     }
   }
+
+  @media (max-width: 768px) {
+    section {
+      padding: 0;
+
+      h1 {
+        font-size: 1.5em;
+        padding: 0.5em;
+      }
+    }
+  }
+
   :global(p) {
     line-height: 1.5em;
     margin: 2em 0;
@@ -43,6 +96,34 @@
   :global(img) {
     max-width: 50%;
     height: auto;
+    transition: transform 0.3s;
+    cursor: pointer;
+  }
+
+  :global(.overlay) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0);
+    z-index: 9999;
+    pointer-events: none;
+    transition: background 0.3s;
+  }
+
+  :global(.overlay.show) {
+    background: rgba(0, 0, 0, 0.2);
+    pointer-events: auto;
+  }
+
+  .expanded-img {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 90%;
+    max-height: 90%;
   }
 
   :global(h1) {
